c65ec79dda2ad9e0e7247ed497effb1f04726408
akmami <akmuhammet99@gmail.com>
2022-09-25 18:34:15 +0300

    About page is implemented

--- src/components/all-tabs/about.js
+++ src/components/all-tabs/about.js
@@ -1,11 +1,49 @@
 import React from "react";
+import about from '../../data/about';
 
 function About() {
     return (
         <div>
-            <label>About Page</label>
+            <div style={styles.div}>
+                <label style={styles.header}>About</label> <br/> <br/>
+            </div>
+            {
+                about ? (
+                    about.map( (par, index) => {
+                        return (
+                            <React.Fragment key={index}>
+                                <p style={styles.p}>&emsp;&emsp;{par}</p> <br/>
+                            </React.Fragment>
+                        )
+                    })
+                ) : (
+                    <div>
+                        <label>Error</label> <br/>
+                    </div>
+                )
+            }
         </div>
+
     )
 }
 
 export default About;
+
+let styles = {
+    header: {
+        width: '100%',
+        fontSize: '1.2em',
+        textAlign: 'center',
+    },
+    div: {
+        width: '100%',
+        minHeight: '100%',
+        overflow: 'hidden',
+        position: 'relative',
+        textAlign: 'center',
+    },
+    p: {
+        marginLeft: '2em',
+        marginRight: '2em',
+    }
+}
---
+++ src/data/about.js
@@ -0,0 +1,14 @@
+const about = [
+    `My name is Akmuhammet Ashyralyyev but you can call me muhammed, akmami, mami, or even aki, ak (due to obvious reasons).
+    I was born in Ashgabat, the capital of Turkmenistan, in 1999. 
+    I went to school no. 51 until we moved to Turkey, Istanbul in 2010. 
+    For 8 years, I have completed my middle school and high school education there. During that time, I have attended many math competitions and won several achievements (thanks to my dad).`,
+    `In 2018, I graduated from Ugur Okulllari and got accepted from Bilkent University with a %100 scholarship.
+    I have learnt many things and developed many skills over 4 years. The online courses that I have completed can be found in the section Courses.
+    Apart from learning, I have tried to put that knowledge into practice. Specifically on courses' projects. The examples can be found on my GitHub repos.
+    I have also got a chance to have a job experience which 2 of them are internship.`,
+    `Lastly, I like travelling, feasting :) and watching movies/series. On the other hand, I am really rigorous (mostly in all aspects of my life). 
+    Sometimes it's a blessing and sometimes its curse :'). But I always find a way to transfer it into something to obtain good results.`
+]
+
+export default about;
