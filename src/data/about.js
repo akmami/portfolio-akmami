a390cc28673a61d9bc5cc661b35ec573a92b02e4
Akmuhammet Ashyralyyev <58183671+akmami@users.noreply.github.com>
2022-11-25 14:10:49 +0300

    Update about.js

--- src/data/about.js
+++ src/data/about.js
@@ -1,14 +1,14 @@
 const about = [
-    `My name is Akmuhammet Ashyralyyev but you can call me muhammed, akmami, mami, or even aki, ak (due to obvious reasons).
+    `My name is Akmuhammet Ashyralyyev but you can call me Muhammed, akmami, mami, or even aki, ak (due to obvious reasons).
     I was born in Ashgabat, the capital of Turkmenistan, in 1999. 
     I went to school no. 51 until we moved to Turkey, Istanbul in 2010. 
-    For 8 years, I have completed my middle school and high school education there. During that time, I have attended many math competitions and won several achievements.`,
-    `In 2018, I graduated from Ugur Okulllari and got accepted from Bilkent University with a %100 scholarship.
-    I have learnt many things and developed many skills over 4 years. The online courses that I have completed can be found in the section Courses.
-    Apart from learning, I have tried to put that knowledge into practice. Specifically on courses' projects. The examples can be found on my GitHub repos.
-    I have also got a chance to have a job experience which 2 of them are internship.`,
-    `Lastly, I like travelling, feasting :) and watching movies/series. On the other hand, I am really rigorous (mostly in all aspects of my life). 
-    Sometimes it's a blessing and sometimes its curse :'). But I always find a way to transfer it into something to obtain good results.`,
+    For 8 years, I have completed my middle and high school education there. During that time, I attended many math competitions and won several achievements.`,
+    `In 2018, I graduated from Ugur Okullari and got accepted by Bilkent University with a %100 scholarship.
+    I have learnt many things and developed many skills over 4 years. The online courses I have completed can be found in the section Courses.
+    Apart from learning, I have tried to put that knowledge into practice, specifically, on course projects. The examples can be found on my GitHub repos.
+    I have also got a chance to have job experiences that 2 of which are internships.`,
+    `Lastly, I like travelling, feasting, and watching movies/series. On the other hand, I am really rigorous (mainly in all aspects of my life). 
+    Sometimes it's a blessing, sometimes it's a curse :'). But I always find a way to transfer it into something to obtain good results.`,
     `Strong: Swift, Python (+Django), Java (+Spring Framework), git, MySQL + OOP`,
     `Middle: C++, JavaScript (React), Heroku`,
     `Limited: RabbitMQ`,
